3ed3e4ff28339ac6728c397636dbd76644244304
Freidch <61506787+Freidch@users.noreply.github.com>
2022-02-18 08:58:29 +0100

    Add files via upload

---
+++ teleconsultation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Clinique Le Chatelet</title>
+    </head>
+
+    <body>
+        <h1 id="nomClinique">Clinique LE CHATELET</h1>
+        <h1>Téléconsultations</h1>
+               
+        <p>
+            Cet espace est réservé aux téléconsultations. 
+        </p>
+        <a href="accueil.php">Retour à la page d'accueil</a>
+    </body>
+</html>
